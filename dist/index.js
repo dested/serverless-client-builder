2d00b9a51016b52f0539cf82cf72db94d576a98c
Salvatore <dested@gmail.com>
2018-12-20 23:25:08 -0700

    better yaml naming and url

--- dist/index.js
+++ dist/index.js
@@ -5,5 +5,5 @@ const yargs = require("yargs");
 const process_1 = require("./process");
 // processFile('c:/code/CleverRX/api/', 'c:/code/CleverRX/app/src/dataServices/app.generated.ts');
 // processFile('c:/code/double-dates/api2/', 'c:/code/double-dates/app/src/dataServices/app.generated.ts');
-process_1.processFile(yargs.argv.in, yargs.argv.out, !!yargs.argv.yml);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFQSwrQkFBK0I7QUFDL0IsdUNBQXNDO0FBRXRDLGtHQUFrRztBQUNsRywyR0FBMkc7QUFFM0cscUJBQVcsQ0FBQyxLQUFLLENBQUMsSUFBSSxDQUFDLEVBQUUsRUFBRSxLQUFLLENBQUMsSUFBSSxDQUFDLEdBQUcsRUFBRSxDQUFDLENBQUMsS0FBSyxDQUFDLElBQUksQ0FBQyxHQUFHLENBQUMsQ0FBQyJ9
+process_1.processFile(yargs.argv.in, yargs.argv.out);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFQSwrQkFBK0I7QUFDL0IsdUNBQXNDO0FBRXRDLGtHQUFrRztBQUNsRywyR0FBMkc7QUFFM0cscUJBQVcsQ0FBQyxLQUFLLENBQUMsSUFBSSxDQUFDLEVBQUUsRUFBRSxLQUFLLENBQUMsSUFBSSxDQUFDLEdBQUcsQ0FBQyxDQUFDIn0=
--- index.ts
+++ index.ts
@@ -6,4 +6,4 @@ import {processFile} from './process';
 // processFile('c:/code/CleverRX/api/', 'c:/code/CleverRX/app/src/dataServices/app.generated.ts');
 // processFile('c:/code/double-dates/api2/', 'c:/code/double-dates/app/src/dataServices/app.generated.ts');
 
-processFile(yargs.argv.in, yargs.argv.out, !!yargs.argv.yml);
+processFile(yargs.argv.in, yargs.argv.out);
