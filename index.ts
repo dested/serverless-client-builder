60446203e41d75dad267b8d375516fcd48c10874
Salvatore <dested@gmail.com>
2019-02-20 22:26:33 -0700

    package json sync command

--- index.ts
+++ index.ts
@@ -1,6 +1,25 @@
 #!/usr/bin/env node
 
+import * as fs from 'fs';
 import * as yargs from 'yargs';
 import {processFile} from './process';
 
-processFile(yargs.argv.in as any, yargs.argv.out as any, yargs.argv.legacyUrl as any, yargs.argv.micro as any);
+const inArg = yargs.argv.in as any;
+const outArg = yargs.argv.out as any;
+if (yargs.argv.syncPackage) {
+  const outerPackage = JSON.parse(fs.readFileSync(inArg + 'package.json', {encoding: 'utf8'}));
+  const dirs = fs.readdirSync(inArg + '/controllers');
+  for (const dir of dirs) {
+    if (dir.indexOf('Controller') >= 0) {
+      const innerPath = inArg + '/controllers/' + dir + '/' + 'package.json';
+      const innerPackage = JSON.parse(fs.readFileSync(innerPath, {encoding: 'utf8'}));
+      innerPackage.dependencies = outerPackage.dependencies;
+      innerPackage.devDependencies = outerPackage.devDependencies;
+      fs.writeFileSync(innerPath, JSON.stringify(innerPackage, null, '  '), {
+        encoding: 'utf8',
+      });
+    }
+  }
+} else {
+  processFile(inArg, outArg, yargs.argv.legacyUrl as any, yargs.argv.micro as any);
+}
